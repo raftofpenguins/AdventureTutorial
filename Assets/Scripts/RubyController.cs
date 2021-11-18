5f02da6c44169bc6b1e7f21c8fce56be7fc68298
Raft of Penguins <raftofpenguins@gmail.com>
2021-11-18 15:18:23 -0800

    Added horizontal movement to character script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,8 +13,17 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;  //Created variable storing Ruby's position
+        // Code from 2nd lesson
+        float horizontal = Input.GetAxis("Horizontal"); // Gets horizontal input and stores in variable
+        Debug.Log(horizontal);                          // Displays horizontal variable in debug log
+        Vector2 position = transform.position;          // Created variable storing Ruby's position
+        position.x = position.x + 0.1f * horizontal;    // Moves game object horizontally
+        transform.position = position;                  // Sets game object's position to new value
+
+        /* Original test code
+        Vector2 position = transform.position;  // Created variable storing Ruby's position
         position.x = position.x + 0.1f;         // Moves game object to the right
         transform.position = position;          // Sets game object's position to new value
+        */
     }
 }
