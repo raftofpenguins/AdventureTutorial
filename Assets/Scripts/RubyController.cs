7d56b5514e0d53fe37993f63689920e76e226e2e
Raft of Penguins <raftofpenguins@gmail.com>
2021-11-18 14:28:47 -0800

    Added movement to Update function

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,6 +13,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;  //Created variable storing Ruby's position
+        position.x = position.x + 0.1f;         // Moves game object to the right
+        transform.position = position;          // Sets game object's position to new value
     }
 }
