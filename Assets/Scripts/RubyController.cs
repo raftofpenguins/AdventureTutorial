785791b2e7d06cc9c04c7ca5ae5e3e60504e9433
Raft of Penguins <raftofpenguins@gmail.com>
2021-12-01 14:38:50 -0800

    Updated controller script for rigid body physics

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,30 +4,54 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    //Added during World Interactions part 9
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal"); // Gets horizontal input and stores in variable
+        vertical = Input.GetAxis("Vertical");     // Gets vertical input and stores in variable
+
         // Code from 2nd lesson
+        /*
         float horizontal = Input.GetAxis("Horizontal"); // Gets horizontal input and stores in variable
         float vertical = Input.GetAxis("Vertical");     // Gets vertical input and stores in variable
+        */
 
         // Debug.Log(horizontal);                       // Displays horizontal variable in debug log
+
+        // Moved to FixedUpdate during later tutorial
+        /*
         Vector2 position = transform.position;          // Created variable storing Ruby's position
         position.x = position.x + 3.0f * Time.deltaTime * horizontal;    // Moves game object horizontally
         position.y = position.y + 3.0f * Time.deltaTime * vertical;      // Moves game object vertically
 
         transform.position = position;                  // Sets game object's position to new value
+        */
 
         /* Original test code
         Vector2 position = transform.position;  // Created variable storing Ruby's position
         position.x = position.x + 0.1f;         // Moves game object to the right
         transform.position = position;          // Sets game object's position to new value
         */
+    }
+
+    // Added during World Interactions tutorial part 9
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;                           // Created variable storing Ruby's position
+        position.x = position.x + 3.0f * Time.deltaTime * horizontal;       // Moves game object horizontally
+        position.y = position.y + 3.0f * Time.deltaTime * vertical;         // Moves game object vertically
+
+        rigidbody2d.MovePosition(position);                                 // Sets game object's position to new value
     }
 }
