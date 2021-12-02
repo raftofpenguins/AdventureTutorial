58543fc1e6281ba74629bbd283431f97eaf1daf9
Raft of Penguins <raftofpenguins@gmail.com>
2021-12-01 17:08:53 -0800

    Added damage zone prefab and script for taking damage upon collision

---
+++ Assets/Scripts/DamageZone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Object that entered trigger: " + other);
+
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+        
+    }
+}
