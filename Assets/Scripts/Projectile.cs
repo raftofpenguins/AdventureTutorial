32ca08e301587197db3a563fc085c2407e9a1f9a
Raft of Penguins <raftofpenguins@gmail.com>
2021-12-04 16:31:47 -0800

    Created cog prefab & projectile script

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    Rigidbody2D cogRigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cogRigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LaunchCog(Vector2 direction, float force)
+    {
+        cogRigidbody2d.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Projectile collided with " + other.gameObject);
+        Destroy(gameObject);
     }
 }
