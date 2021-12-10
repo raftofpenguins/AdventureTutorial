3edb32da063cebe029ad1524947618abf35063c9
Raft of Penguins <raftofpenguins@gmail.com>
2021-12-10 11:56:43 -0800

    Added talk function and dialog canvas

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -65,6 +65,17 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
+        //Added during Dialog tutorial
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+            }
+        }
+
         //Debug.Log(lookDirection);
 
         /*
